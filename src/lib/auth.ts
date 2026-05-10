7d1e54f5900a1e26b1e9c0c8bdd96e989143663f
Daniel <danielnguyen5201@gmail.com>
2026-05-10 10:26:17 +0700

    config betterauth dashboard

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
+import { admin } from 'better-auth/plugins/admin'
 import { db } from '@/db'
 import * as schema from '@/db/schema'
 
@@ -23,5 +24,10 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     },
   },
-  plugins: [nextCookies()],
+  plugins: [
+    nextCookies(),
+    admin({
+      adminRoles: ['admin'],
+    }),
+  ],
 })
